e9103ccfff6f3f6d1d9745af56d4145de1c85070
David Goeth <goeth@fim.uni-passau.de>
2018-03-10 21:27:35 +0100

    Cleaned DemoApp

--- DemoApp/decompileOAT/dumpCommands.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh 
-cd /sdcard/test
-dex2oat --dex-file=EvilModule.dex --oat-file=EvilModule.oat 
-oatdump --oat-file=EvilModule.oat --output=EvilModule.dump
--- DemoApp/decompileOAT/makeDump.bat
+++
@@ -1,5 +0,0 @@
-adb root
-adb push dumpCommands.sh /sdcard/test/dumpCommands.sh
-adb push EvilModule.dex /sdcard/test/EvilModule.dex
-adb shell sh /sdcard/test/dumpCommands.sh
-adb pull /sdcard/test/EvilModule.dump EvilModule.dump
